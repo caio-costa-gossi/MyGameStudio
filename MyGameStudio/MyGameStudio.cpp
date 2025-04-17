d47802031c2c64cebafbf2ff42b3d589f0a0e7e9
caio-gossi <148551966+caio-gossi@users.noreply.github.com>
2025-04-17 02:26:28 -0300

    Clean-up

--- MyGameStudio/MyGameStudio.cpp
+++ MyGameStudio/MyGameStudio.cpp
@@ -25,12 +25,6 @@ int main()
 		std::cout << "Error: " << err;
 	}
 
-	char* myBuffer = new char[2048];
-	char* buffer2 = new char[10000];
-
-	const FileIoTaskJanitor task = FileManager::ReadFileAsync(1, "config.ini", &myBuffer, 2048, MyCallback);
-	const FileIoTaskJanitor task2 = FileManager::ReadFileAsync(2, "strings.csv", &buffer2, 10000, MyCallback);
-
 	err = EngineCoreManager::Shutdown();
 	if (err.Code() != 0)
 	{

40bcdb8babaef934b7a1fdad8566e8cb9c65d72f
caio-gossi <148551966+caio-gossi@users.noreply.github.com>
2025-04-25 03:15:25 -0300

    Configuring project with vcpkg and zlib

--- MyGameStudio/MyGameStudio.cpp
+++ MyGameStudio/MyGameStudio.cpp
@@ -1,9 +1,12 @@
+#include <cassert>
+
 #include "AssetDatabase.h"
 #include "CommandFactory.h"
 #include "ConsoleManager.h"
 #include "EngineCoreManager.h"
 #include "Err.h"
 #include "SqliteDatabase.h"
+#include <zlib.h>
 
 int main()
 {
@@ -20,14 +23,6 @@ int main()
 	}
 
 	//ConsoleManager::RunConsole();
-
-	std::vector<uint32_t> dependencies = { 3,4 };
-	//Asset asset{ 0, "myAsset.png", "png", enums::AssetType::image, "C:/Downloads/myAsset.png", "Assets/myAssetLegal.zip", "2025-02-01 00:00:00", false };
-	Asset asset{ 0, "meuAsset.mat", "mat", enums::AssetType::material, "C:/Downloads/myMaterial.mat", "Assets/myAssetMaterial.zip", "2025-01-01 00:00:00", true };
-	//Asset asset = AssetDatabase::GetAsset(1);
-	//asset.DependsOnAssets = dependencies;
-
-	AssetDatabase::DeleteAsset(0);
 
 	err = EngineCoreManager::Shutdown();
 	if (err.Code() != 0)

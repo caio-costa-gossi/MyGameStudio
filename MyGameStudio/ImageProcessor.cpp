54fac6fbebc1529653c00ba23e7b382412d8e34a
caio-gossi <148551966+caio-gossi@users.noreply.github.com>
2025-04-30 02:15:37 -0300

    Implement mipmap creation process and compression

--- MyGameStudio/ImageProcessor.cpp
+++ MyGameStudio/ImageProcessor.cpp
@@ -1,16 +1,80 @@
 #include "ImageProcessor.h"
+#include <squish.h>
 
 #define STB_IMAGE_IMPLEMENTATION
 #include <stb_image.h>
 
-ImageProcessor::ImageProcessor()
-{
-	
-}
-
 uint8_t* ImageProcessor::DecompressImageRgba8(const char* filepath, int* x, int* y, int* channels)
 {
 	return stbi_load(filepath, x, y, channels, 4);
+}
+
+uint8_t* ImageProcessor::Downscale2X(const uint8_t* source, const uint64_t srcX, const uint64_t srcY)
+{
+	const uint64_t destX = srcX >> 1ULL;
+	const uint64_t destY = srcY >> 1ULL;
+
+	const auto destBuffer = new uint8_t[destX * destY * 4];
+
+	for (uint64_t x = 0; x < destX; ++x)
+	{
+		for (uint64_t y = 0; y < destY; ++y)
+		{
+			const uint8_t* p1 = &source[(x * srcY + y) * 4];
+			const uint8_t* p2 = &source[((x + 1) * srcY + y) * 4];
+			const uint8_t* p3 = &source[(x * srcY + (y + 1)) * 4];
+			const uint8_t* p4 = &source[((x + 1) * srcY + (y + 1)) * 4];
+
+			for (int i = 0; i < 4; ++i)
+			{
+				destBuffer[x * destY + y + i] = 
+					(	static_cast<int>(p1[i]) + 
+						static_cast<int>(p2[i]) + 
+						static_cast<int>(p3[i]) + 
+						static_cast<int>(p4[i])) / 4;
+			}
+		}
+	}
+
+	return destBuffer;
+}
+
+void ImageProcessor::GenerateMipmaps(std::vector<Mipmap>& mipmaps, uint64_t paddedX, uint64_t paddedY, uint8_t* paddedBuffer)
+{
+	mipmaps.reserve(8);
+
+	Mipmap lvl0(0, paddedX, paddedY, paddedX * paddedY * 4ULL);
+	memcpy_s(lvl0.Data, lvl0.DataSize, paddedBuffer, paddedX * paddedY * 4);
+	mipmaps.emplace_back(lvl0);
+
+	uint8_t curLvl = 1;
+
+	while (paddedX != 1 && paddedY != 1)
+	{
+		paddedX = paddedX >> 1;
+		paddedY = paddedY >> 1;
+
+		Mipmap nextLvl(curLvl, paddedX, paddedY, paddedX * paddedY * 4ULL);
+		nextLvl.Data = Downscale2X(mipmaps.back().Data, paddedX * 2, paddedY * 2);
+		mipmaps.emplace_back(nextLvl);
+
+		curLvl++;
+	}
+}
+
+void ImageProcessor::CompressMipmaps(const std::vector<Mipmap>& mipmaps, std::vector<Mipmap>& compressedMipmaps)
+{
+	for (const Mipmap& m : mipmaps)
+	{
+		Mipmap compressed(m.Level, m.XSize, m.YSize, squish::GetStorageRequirements(m.XSize, m.YSize, squish::kDxt5), true);
+		squish::CompressImage(m.Data, m.XSize, m.YSize, compressed.Data, squish::kDxt5);
+		compressedMipmaps.emplace_back(compressed);
+	}
+}
+
+uint8_t* ImageProcessor::GenerateTexFile(const std::vector<Mipmap>& compressedMipmaps)
+{
+
 }
 
 uint8_t* ImageProcessor::ProcessImage(const Asset& metadata)
@@ -33,15 +97,15 @@ uint8_t* ImageProcessor::ProcessImage(const Asset& metadata)
 
 	// Generate mipmaps and delete padded buffer
 	std::vector<Mipmap> mipmaps;
-	/*GenerateMipmaps(mipmaps, paddedX, paddedY, paddedBuffer);
+	GenerateMipmaps(mipmaps, paddedX, paddedY, paddedBuffer);
 	delete[] paddedBuffer;
 
 	// Compress mipmaps
 	std::vector<Mipmap> compressedMipmaps;
-	CompressMipmaps(mipmaps, compressedMipmaps);*/
+	CompressMipmaps(mipmaps, compressedMipmaps);
 
 	// Generate .tex file
-	uint8_t* finalProduct = new uint8_t[1]; // GenerateTexFile();
+	uint8_t* finalProduct = GenerateTexFile(compressedMipmaps);
 
 	return finalProduct;
 }
--- MyGameStudio/ImageProcessor.h
+++ MyGameStudio/ImageProcessor.h
@@ -8,10 +8,11 @@ struct Mipmap
 	uint64_t XSize;
 	uint64_t YSize;
 	uint8_t* Data;
+	uint64_t DataSize;
 
-	explicit Mipmap(const uint8_t level, const uint64_t xSize, const uint64_t ySize, const bool isCompressed = false, const uint64_t memorySize = 0) : Level(level), XSize(xSize), YSize(ySize)
+	explicit Mipmap(const uint8_t level, const uint64_t xSize, const uint64_t ySize, const int dataSize, const bool isCompressed = false) : Level(level), XSize(xSize), YSize(ySize), DataSize(dataSize)
 	{
-		Data = isCompressed ? new uint8_t[memorySize] : new uint8_t[XSize * YSize * 4];
+		Data = isCompressed ? new uint8_t[dataSize] : nullptr;
 	}
 
 	~Mipmap()
@@ -24,11 +25,14 @@ class ImageProcessor
 {
 public:
 	static uint8_t* DecompressImageRgba8(const char* filepath, int* x, int* y, int* channels);
+	static void GenerateMipmaps(std::vector<Mipmap>& mipmaps, uint64_t paddedX, uint64_t paddedY, uint8_t* paddedBuffer);
+	static void CompressMipmaps(const std::vector<Mipmap>& mipmaps, std::vector<Mipmap>& compressedMipmaps);
+	static uint8_t* GenerateTexFile(const std::vector<Mipmap>& compressedMipmaps);
 
 	static uint64_t NextPoT(uint64_t x);
+	static uint8_t* Downscale2X(const uint8_t* source, uint64_t srcX, uint64_t srcY);
 
 public:
-	ImageProcessor();
 
-	uint8_t* ProcessImage(const Asset& metadata);
+	static uint8_t* ProcessImage(const Asset& metadata);
 };

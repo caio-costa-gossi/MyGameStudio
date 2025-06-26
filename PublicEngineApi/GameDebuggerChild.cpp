d60fd3aa05d027cc7c34917d0cfbdd370b5e51e5
caio-gossi <148551966+caio-gossi@users.noreply.github.com>
2025-06-26 19:52:21 -0300

    Implement debugger screen for input

--- PublicEngineApi/GameDebuggerChild.cpp
+++ PublicEngineApi/GameDebuggerChild.cpp
@@ -61,7 +61,36 @@ Err GameDebuggerChild::Shutdown()
 void GameDebuggerChild::GetInputDebugInfo()
 {
 	const InputState currentState = InputManager::GetInputState();
-	debugInfo_ = std::bitset<32>(currentState.Gamepads[0].State.BtnState).to_string();
+	debugInfo_ = "";
+
+	// Gamepads
+	for (uint8_t padIndex = 0; padIndex < currentState.GamepadCount; ++padIndex)
+	{
+		const Gamepad gamepad = currentState.Gamepads[padIndex];
+		debugInfo_ += "====GAMEPAD " + std::to_string(padIndex) + "====\n\n";
+
+		// Buttons
+		debugInfo_ += "Buttons: " + std::bitset<32>(gamepad.State.BtnState).to_string();
+		debugInfo_ += "\n";
+
+		// Axes
+		for (uint8_t axisId = 0; axisId < gamepad_axis_count; ++axisId)
+			debugInfo_ += "Axis " + std::to_string(axisId) + ": " + std::to_string(gamepad.State.AxisState[axisId]) + " || ";
+	}
+
+	debugInfo_ += "\n";
+
+	// Keyboard
+	debugInfo_ += "\n====KEYBOARD====\n\n";
+	for (const bool keyState : currentState.KeyboardState.RawState)
+		debugInfo_ += std::to_string(keyState);
+
+	// Mouse
+	debugInfo_ += "\n\n====MOUSE====\n\n";
+	debugInfo_ += "Buttons: " + std::bitset<8>(currentState.MouseState.BtnState).to_string();
+	debugInfo_ += "\nXPos: " + std::to_string(currentState.MouseState.XPos) + " || YPos: " + std::to_string(currentState.MouseState.YPos);
+	debugInfo_ += "\nXVel: " + std::to_string(currentState.MouseState.XVel) + " || YVel: " + std::to_string(currentState.MouseState.YVel);
+	debugInfo_ += "\nWheel XVel: " + std::to_string(currentState.MouseState.WheelXVel) + " || Wheel YVel: " + std::to_string(currentState.MouseState.WheelYVel);
 }
 
 
